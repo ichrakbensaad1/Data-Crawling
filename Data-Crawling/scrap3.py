d8be27cd7f067de532759903412e7d3e565d510c
Wissal Farjallah <wissal.farjallah@gmail.com>
2022-08-10 17:32:30 +0100

    HHS.gov scraping

--- Data-Crawling/scrap3.py
+++ Data-Crawling/scrap3.py
@@ -2,7 +2,7 @@ from bs4 import BeautifulSoup
 import requests
 from csv import writer
 
-from scrap import Locations
+#from scrap import Locations
 
 import urllib3
 urllib3.disable_warnings()
@@ -11,14 +11,24 @@ url= "https://www.hhs.gov/ohrp/international/clinical-trial-registries/index.htm
 
 page = requests.get(url, verify=False)
 soup = BeautifulSoup(page.content, 'html.parser')
-lists = soup.find_all('tbody').find('tr')
+lists = soup.find_all('tbody')
 
 with open('dataset.csv', 'w', encoding='utf8', newline='') as f:
     thewriter = writer(f)
     header = ['Trial_ID']
     thewriter.writerow(header)
-    info = [Trial_ID,'\n']
-    thewriter.writerow(info)   
+    #info = [Trial_ID,'\n']
+    #thewriter.writerow(info)   
+
+    for list in lists:
+        result = list.find_all('tr')
+        for res in result:
+            res.find('td')
+            #print(res)
+            info = [res.getText(),'\n']
+            thewriter.writerow(info)
+
+
     #for list in lists:
         #Trial_ID = list.find('tr').text.replace('\n', '')
         #Trial_ID = list.find('tr').text.replace('\n', '')
